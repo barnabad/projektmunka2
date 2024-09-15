d7888fb60b27c4155c710b0242133ad83c0b1e1a
Alex Nagy <91522480+Dumach@users.noreply.github.com>
2024-09-15 16:47:24 +0200

    Renamed project folders, added websocket to server.js

---
+++ skribble-backend/server.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import { Server } from 'socket.io';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Getting project directory name
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const PORT = process.env.PORT || 3000;
+
+// Creating express server
+const app = express();
+
+// Currently not working
+app.use(express.static(path.join(__dirname, '../skribble-client/dist')));
+
+const expressServer = app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
+// Creating websocket
+const io = new Server(expressServer, {
+  cors: {
+    origin: process.env.NODE_ENV === 'production' ? false:
+    ['http://localhost:5173', 'http://127.0.0.1:5173']
+  }
+});
+
+io.on('connection', socket => {
+  console.log(`User ${socket.id} connected`);
+
+  // Upon connection - only to user
+  socket.emit('message', 'Welcome User!');
+
+  // Upon connection - to all others
+  socket.broadcast.emit('meesage', `User ${socket.id.substring(0, 5)} connected`)
+
+  // Listening for a message event
+  socket.on('message', data => {
+    console.log(data);
+    io.emit('message', `${socket.id.substring(0, 5)}: ${data}`);
+  })
+
+  // When user disconnects - to all others
+  socket.on.apply('disconnect', () =>{
+    socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} disconnected`)
+  })
+
+  // Listen for activity
+  socket.on('activity', (name) => {
+    socket.broadcast.emit('activity', name)
+  })
+});
