c8b3d9e823daa54f5157305e4e377de17186bf55
Alex Nagy <nagyalex003@gmail.com>
2024-11-22 21:31:51 +0100

    HTTPS connection, dockerfile

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:3000";
+const URL = "https://localhost:3000";
 
 export const socket = io(URL, {
   autoConnect: true,
+  secure: true,
 });
--- server/src/config/socketConfig.ts
+++ server/src/config/socketConfig.ts
@@ -2,7 +2,8 @@ export const socketConfig = {
   cors: {
     origin:
       process.env.NODE_ENV === "production"
-        ? false
+        ? ["https://localhost:3000", "https://127.0.0.1:3000"]
         : ["http://localhost:5173", "http://127.0.0.1:5173"],
+    credentials: true,
   },
 };
