3695460a9c08fbbe2c333f98d0aed531c77a918f
Alex Nagy <nagyalex003@gmail.com>
2024-12-17 19:46:14 +0100

    Added meta description for better SEO, fixed package vulnerability, better dev support

--- server/src/config/socketConfig.ts
+++ server/src/config/socketConfig.ts
@@ -5,9 +5,13 @@ export const socketConfig = {
         ? false
         : [
             "http://localhost:5173",
+            "https://localhost:5173",
             "http://127.0.0.1:5173",
+            "https://127.0.0.1:5173",
             "http://localhost:3000",
+            "https://localhost:3000",
             "http://127.0.0.1:3000",
+            "https://127.0.0.1:3000",
           ],
   },
 };
